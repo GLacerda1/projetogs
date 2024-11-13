9568e3c54433b69027b15879e95f8fb59e05fb63
Gabriel Luni Nakashima <46712524+GabrielNakashima@users.noreply.github.com>
2024-11-13 17:13:45 -0300

    Alterando o footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,6 @@
-const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Footer =()=>{
+    return (
+        <h5>@2024-todos os direitos reservados</h5>
+    )
 }
-
 export default Footer

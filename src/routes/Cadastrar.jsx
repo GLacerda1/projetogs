063449eda9f6618d66155debaaf40ab108048e4a
Gabriel Luni Nakashima <46712524+GabrielNakashima@users.noreply.github.com>
2024-11-13 16:41:59 -0300

    Criando página cadastrar

--- src/routes/Cadastrar.jsx
+++ src/routes/Cadastrar.jsx
@@ -1,9 +1,84 @@
-function Cadastrar() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import {useParams,Link, useNavigate} from 'react-router-dom'
+import { ImCancelCircle } from "react-icons/im";
+import { useState,useEffect } from 'react';
 
-export default Cadastrar
+
+const CadastrarUsuario=()=>{
+
+    let {id} =useParams();
+    const [usuarios,setUsuarios]= useState({
+        id,
+        usuario:'',
+        senha:'',
+    });
+
+     const navigate = useNavigate();
+
+    //criando uma variavel com post/put
+    let metodo ="post";
+    if(id){
+        metodo="put";
+    }
+     const handleChange=(e)=>{
+        setUsuarios({...usuarios, [e.target.name]: e.target.value});
+     }
+
+     const handleSubmit=(e)=>{
+        e.preventDefault();
+        fetch(`http://localhost:5000/usuarios/${id ? id:''}`,{
+        method: metodo,
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(usuarios),
+        }).then(()=> {
+            navigate("/login")
+        });
+     }
+
+     useEffect(()=>{
+        if(id) {
+        fetch(`http://localhost:5000/usuarios/${id}`)
+        .then((res)=>{
+            return res.json();
+        })
+        .then((data)=>{
+            setUsuarios(data);
+        });
+    }
+     },[id])
+
+
+    return (
+        <section className="usuario">
+
+            <form onSubmit={handleSubmit}>
+                <h1>Cadastrar Usuário</h1>
+                <input
+                    type="text"
+                    name="usuario"
+                    placeholder="Digite seu Usuário"
+                    value={usuarios.usuario}
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="password"
+                    name="senha"
+                    placeholder="Digite sua senha"
+                    value={usuarios.senha}
+                    onChange={handleChange}
+                />
+
+                <button type="submit">Cadastrar</button>
+                <Link to="/login">
+                    <ImCancelCircle />
+                </Link>
+
+
+
+            </form>
+        </section>
+    )
+}
+export default CadastrarUsuario
